93aeb5bcaa40939ac9b8b7d85d340f3d6ab34f78
Axel Froborg <axel.froborg@gmail.com>
2023-12-09 15:21:46 +0100

    day 9 part2

--- day-09/src/part2.rs
+++ day-09/src/part2.rs
@@ -1,5 +1,32 @@
 pub fn process(input: &str) -> String {
-    todo!("Write code here")
+    let input_lines = input.lines().map(|line| {
+        line.split_whitespace()
+            .map(|n| n.parse::<i32>().expect("Should be a number"))
+            .collect::<Vec<_>>()
+    });
+
+    let remainders = input_lines
+        .map(|mut numbers| {
+            let mut ends = vec![numbers.first().unwrap().to_owned()];
+
+            loop {
+                if numbers.iter().all(|n| n == &0) {
+                    break;
+                }
+
+                numbers = numbers
+                    .windows(2)
+                    .map(|pair| pair[0] - pair[1])
+                    .collect::<Vec<_>>();
+
+                ends.push(numbers.first().unwrap().to_owned());
+            }
+
+            ends.iter().sum::<i32>()
+        })
+        .sum::<i32>();
+
+    remainders.to_string()
 }
 
 #[cfg(test)]
@@ -8,8 +35,9 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Write tests for process function");
-        let input = "";
-        assert_eq!(process(input), "");
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(process(input), "2");
     }
 }
