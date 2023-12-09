847d0db28416cdd9dc0058e3959078b0b4cc77d0
Axel Froborg <axel.froborg@gmail.com>
2023-12-09 15:11:07 +0100

    day09 part1

---
+++ day-09/src/part1.rs
@@ -0,0 +1,43 @@
+pub fn process(input: &str) -> String {
+    let input_lines = input.lines().map(|line| {
+        line.split_whitespace()
+            .map(|n| n.parse::<i32>().expect("Should be a number"))
+            .collect::<Vec<_>>()
+    });
+
+    let remainders = input_lines
+        .map(|mut numbers| {
+            let mut ends = vec![numbers.last().unwrap().to_owned()];
+
+            loop {
+                if numbers.iter().all(|n| n == &0) {
+                    break;
+                }
+
+                numbers = numbers
+                    .windows(2)
+                    .map(|pair| pair[1] - pair[0])
+                    .collect::<Vec<_>>();
+
+                ends.push(numbers.last().unwrap().to_owned());
+            }
+
+            ends.iter().sum::<i32>()
+        })
+        .sum::<i32>();
+
+    remainders.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(process(input), "114");
+    }
+}
---
+++ day-09/src/part2.rs
@@ -0,0 +1,15 @@
+pub fn process(input: &str) -> String {
+    todo!("Write code here")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        todo!("Write tests for process function");
+        let input = "";
+        assert_eq!(process(input), "");
+    }
+}
