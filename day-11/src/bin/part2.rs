78aab363d0561d6f0e6455464a9fbc28c78d7179
Axel Froborg <axel.froborg@gmail.com>
2023-12-13 11:00:17 +0100

    day11 done

--- day-11/src/bin/part2.rs
+++ day-11/src/bin/part2.rs
@@ -3,6 +3,6 @@ use day_11::part2::process;
 fn main() {
     let file = include_str!("../../input2.txt");
     let timing = std::time::Instant::now();
-    println!("{}", process(file));
+    println!("{}", process(file, 1000000));
     println!("Elapsed: {}ms", timing.elapsed().as_millis());
 }
--- day-11/src/part2.rs
+++ day-11/src/part2.rs
@@ -1,5 +1,80 @@
-pub fn process(input: &str) -> String {
-    todo!("Write code here")
+use std::collections::HashSet;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Tile {
+    Empty,
+    Galaxy,
+}
+
+impl From<char> for Tile {
+    fn from(c: char) -> Self {
+        match c {
+            '.' => Self::Empty,
+            '#' => Self::Galaxy,
+            _ => panic!("Invalid tile"),
+        }
+    }
+}
+
+pub fn process(input: &str, expansion_amount: i64) -> String {
+    let mut galaxies = vec![];
+    let mut empty_rows: HashSet<i64> = HashSet::new();
+    let mut empty_cols: HashSet<i64> = HashSet::new();
+
+    let tiles = input
+        .lines()
+        .enumerate()
+        .map(|(y, line)| {
+            let tiles = line
+                .chars()
+                .enumerate()
+                .map(|(x, c)| {
+                    let tile = Tile::from(c);
+
+                    if let Tile::Galaxy = tile {
+                        galaxies.push((x as i64, y as i64));
+                    }
+
+                    tile
+                })
+                .collect::<Vec<_>>();
+
+            if tiles.iter().all(|t| matches!(t, Tile::Empty)) {
+                empty_rows.insert(y as i64);
+            }
+
+            tiles
+        })
+        .collect::<Vec<_>>();
+
+    for x in 0..tiles[0].len() {
+        if tiles.iter().all(|row| matches!(row[x], Tile::Empty)) {
+            empty_cols.insert(x as i64);
+        }
+    }
+
+    let sum = galaxies.iter().enumerate().map(|(i, (x, y))| {
+        (i..galaxies.len()).fold(0, |acc2, j| {
+            let (x2, y2) = galaxies[j];
+            let mut dij = (y2 - y).abs() + (x2 - x).abs();
+
+            for er in empty_rows.iter() {
+                if y.min(&y2) < er && er < y.max(&y2) {
+                    dij += expansion_amount - 1;
+                }
+            }
+
+            for ec in empty_cols.iter() {
+                if x.min(&x2) < ec && ec < x.max(&x2) {
+                    dij += expansion_amount - 1;
+                }
+            }
+
+            acc2 + dij
+        })
+    });
+
+    sum.sum::<i64>().to_string()
 }
 
 #[cfg(test)]
@@ -8,8 +83,31 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Write tests for process function");
-        let input = "";
-        assert_eq!(process(input), "");
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+        assert_eq!(process(input, 10), "1030");
+    }
+
+    #[test]
+    fn test_process2() {
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+        assert_eq!(process(input, 100), "8410");
     }
 }
